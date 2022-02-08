32d0fe453631bcd662b032af9553a9562bd94ebb
CChevr <cedricus@msn.com>
2022-02-08 22:55:11 +0100

    udpate

--- chap-05/2-sequentials.cpp
+++ chap-05/2-sequentials.cpp
@@ -1,4 +1,9 @@
+#include <array>
+#include <iostream>
 #include <list>
+#include <stack>
+#include <string>
+#include <vector>
 
 enum Fruit
 {
@@ -12,20 +17,55 @@ enum Fruit
 
 void try_arrays()
 {
-    // Implement array tests here.
+    std::array<std::string, Fruit_Count> fruits_name;
+    fruits_name[Apricot]   = "Apricot";
+    fruits_name[Cherry]    = "Cherry";
+    fruits_name[Mango]     = "Mango";
+    fruits_name[Raspberry] = "Raspberry";
 }
 
 void try_lists()
 {
-    std::list<int> l1;
-    std::list<int> l2;
+    std::list<int> l1 { -2, -3, 7, 200, -8, 6 };
+    std::list<int> l2(3, -45);
+
     std::list<int> l3;
+    l3.assign({ 41, 6, 12 });
+
     std::list<int> l4;
+    l4.emplace_front(0);
+    l4.emplace_front(4);
+    l4.emplace_front(6);
+
+    // First possibility : sort first, then use merge.
+    l1.sort();
+    l2.sort();
+    l1.merge(l2);
+
+    // Second possibility : append one to the other, then use sort.
+    l3.splice(l3.begin(), l4);
+    l3.sort();
+
+    // Find the middle of the list by using function std::advance, and insert the other list at this position.
+    auto mid = l1.begin();
+    std::advance(mid, l1.size() / 2);
+    l1.splice(mid, l3);
 }
 
 void try_stacks()
 {
-    // Implement stack tests here.
+    std::stack<int, std::vector<int>> stack;
+    stack.emplace(0);
+    stack.emplace(1);
+    stack.emplace(2);
+
+    std::vector<int>                  v { 0, 1, 2 };
+    std::stack<int, std::vector<int>> s2 { v };
+
+    for (auto e : stack)
+    {
+        std::cout << e << std::endl;
+    }
 }
 
 int main()
--- chap-05/3-associatives/main.cpp
+++ chap-05/3-associatives/main.cpp
@@ -1,4 +1,42 @@
+#include "keys.h"
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <unordered_set>
+
+using Owner = std::pair<std::string, std::string>;
 int main()
 {
+    std::unordered_set<HashableDog> cds;
+    HashableDog                     dog { "Bull", "Yorkshire" };
+    HashableDog                     dog2 { "Milou", "Whisky" };
+    // auto                            copy_dog = dog;
+
+    cds.emplace(dog);
+    cds.emplace(dog2);
+    // cds.emplace(copy_dog);
+
+    std::cout << cds.size() << std::endl;
+
+    std::map<ComparableDog, Owner> owners_by_dog;
+
+    ComparableDog medor { "medor", "labrador" };
+    ComparableDog gus { "gus", "bordercollie" };
+    ComparableDog zim { "zim", "poodle" };
+    ComparableDog flippy { "flippy", "spaniel" };
+
+    owners_by_dog.insert(std::make_pair(medor, Owner { "Claire", "David" }));
+    owners_by_dog.insert_or_assign(gus, Owner { "Marc", "Zipstein" });
+    owners_by_dog.emplace(zim, Owner { "Céline", "Noël" });
+    owners_by_dog.try_emplace(flippy, "Vincent", "Nozick");
+
+    owners_by_dog.emplace(gus, Owner { "Claire", "David" });
+    // -> gus is still owned by Marc (emplace does not modify values with existing keys, neither does insert
+    // or try_emplace)
+
+    owners_by_dog.insert_or_assign(gus, Owner { "Vincent", "Nozick" });
+    // -> gus is now owned by Vincent (insert_or_assign reassign values with existing keys)
+
     return 0;
 }
